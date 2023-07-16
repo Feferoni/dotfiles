025125c7165982c6ade917be2eabd9cf11412eb7
Olle <ollelundmark89@gmail.com>
2023-07-16 17:31:27 +0200

    Removed recommended lsp preset
    Since it had a lot of default keybinds that were messing with the
    debugger keybinds.
    
    Also moved around some keybinds.

--- nvim/after/plugin/debugging.lua
+++ nvim/after/plugin/debugging.lua
@@ -41,6 +41,12 @@ dap.listeners.before.event_exited["dapui_config"] = function()
     dapui.close()
 end
 
+vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<F1>", ":lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<F2>", ":lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F3>", ":lua require'dap'.step_out()<CR>")
+vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
+vim.keymap.set("n", "<leader>td", ":lua require'dap-go'.debug_test()<CR>")
 
--- require("dap-go").setup()
--- require("dapui").setup()
--- nvim/lua/olle/remap.lua
+++ nvim/lua/olle/remap.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- 
-vim.keymap.set("n", "<F3>", ":set wrap!<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F6>", ":set wrap!<cr>", { noremap = true, silent = true })
 
 -- Move selected block, auto indent if moved to inside a if block
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -21,14 +21,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Let search terms stay in the middle of screen
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
--- Good times on twitch, don't know what this is from, figure out later?
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
 
 -- deletes the currently marked thing to the void register, then copies the new thing into that, will keep the old copy buffer after the delete
 vim.keymap.set("x", "<leader>p", [["_dP]])
@@ -70,18 +62,9 @@ vim.keymap.set("i", "<M-,>", "copilot#Previous()", {expr=true, silent=true})
 vim.keymap.set("i", "<M-->", "copilot#Dismiss()", {expr=true, silent=true})
 vim.keymap.set("i", "<M-m>", "copilot#Suggest()", {expr=true, silent=true})
 
--- source object?
+-- source object
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
     vim.print("Resourced file")
 end)
 
--- debugging
-vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
-vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
-vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>")
-vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>")
-vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
-vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
-vim.keymap.set("n", "<leader>td", ":lua require'dap-go'.debug_test()<CR>")
