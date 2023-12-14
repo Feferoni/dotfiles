c95f84f536ccefbda6e125ef464c0a5abdb80053
Olle <ollelundmark89@gmail.com>
2023-12-14 10:03:14 +0100

    Changing settings for file-tree

--- nvim/after/plugin/file-tree.lua
+++ nvim/after/plugin/file-tree.lua
@@ -1,17 +1,40 @@
 local tree = require('nvim-tree')
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.keymap.set("n", "<leader>et", function ()
     vim.cmd("NvimTreeFindFileToggle")
 end)
 
 tree.setup({
     view = {
-        width = 30,
+        number = true,
+        relativenumber = true,
+        width = 50,
+        float = {
+            quit_on_focus_loss = true,
+        },
     },
     renderer = {
         group_empty = true,
     },
     filters = {
         dotfiles = false,
+        git_clean = false,
+        custom = {},
+        exclude = {},
+    },
+    git = {
+        enable = true,
+        ignore = false,
+        show_on_dirs = true,
+        disable_for_dirs = {},
+        timeout = 400,
+    },
+    update_focused_file = {
+        enable = true,
+        update_root = false,
+        ingore_list = {},
     },
 })
