51a8b75e3251d10638a390382d1f4ae4fa02d71c
Olle <ollelundmark89@gmail.com>
2023-12-19 21:05:26 +0100

    Fixing dap

--- nvim/after/plugin/debugging.lua
+++ nvim/after/plugin/debugging.lua
@@ -1,32 +1,73 @@
 require("nvim-dap-virtual-text").setup()
 require("dap-go").setup()
 require("dapui").setup({
-    icons = { expanded = "▾", collapsed = "▸", circular = "↺" },
+    controls = {
+      element = "repl",
+      enabled = true,
+      icons = {
+        disconnect = "",
+        pause = "",
+        play = "",
+        run_last = "",
+        step_back = "",
+        step_into = "",
+        step_out = "",
+        step_over = "",
+        terminate = ""
+      }
+    },
+    element_mappings = {},
+    expand_lines = true,
+    floating = {
+      border = "single",
+      mappings = {
+        close = { "q", "<Esc>" }
+      }
+    },
+    force_buffers = true,
+    icons = {
+      collapsed = "",
+      current_frame = "",
+      expanded = ""
+    },
+    layouts = { {
+        elements = { {
+            id = "scopes",
+            size = 0.25
+          }, {
+            id = "breakpoints",
+            size = 0.25
+          }, {
+            id = "stacks",
+            size = 0.25
+          }, {
+            id = "watches",
+            size = 0.25
+          } },
+        position = "right",
+        size = 150
+      }, {
+        elements = { {
+            id = "repl",
+            size = 0.5
+          }, {
+            id = "console",
+            size = 0.5
+          } },
+        position = "bottom",
+        size = 10
+      } },
     mappings = {
-        -- Use a table to apply multiple mappings
-        -- expand = { "<CR>", "<2-LeftMouse>" },
-        -- open = "o",
-        -- remove = "d",
-        -- edit = "e",
-        -- repl = "r",
+      edit = "e",
+      expand = { "<CR>", "<2-LeftMouse>" },
+      open = "o",
+      remove = "d",
+      repl = "r",
+      toggle = "t"
     },
-    sidebar = {
-        open_on_start = true,
-        elements = {
-            -- You can change the order of elements in the sidebar
-            "scopes",
-            "breakpoints",
-            "stacks",
-            "watches",
-        },
-        width = 40,
-        position = "left", -- Can be "left" or "right"
-    },
-    tray = {
-        open_on_start = true,
-        elements = { "repl" },
-        height = 10,
-        position = "bottom", -- Can be "bottom" or "top"
+    render = {
+      indent = 1,
+      max_value_lines = 100
     },
 })
 
--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -124,7 +124,10 @@ lsp.on_attach(function(_, bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    nmap('rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('rn', function ()
+        vim.lsp.buf.rename()
+        vim.cmd("wa")
+    end, '[R]e[n]ame')
     nmap('ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
     nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
     nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
@@ -133,10 +136,9 @@ lsp.on_attach(function(_, bufnr)
     nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-    -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-    -- Lesser used LSP functionality
+
     nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
     nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
@@ -163,6 +165,18 @@ lsp.setup()
 
 
 vim.diagnostic.config({
-    virtual_text = true
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        boarder = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+    },
 })
 
