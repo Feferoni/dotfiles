b618bbcb88d460f6797d75a4d85648e1629e52af
Olle <ollelundmark89@gmail.com>
2024-01-04 13:31:35 +0100

    Ading telescope resume hotkey and fuzzy filter

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -37,6 +37,7 @@ require('telescope').setup {
                 ['<C-d>'] = false,
                 ["<C-j>"] = actions.move_selection_next,
                 ["<C-k>"] = actions.move_selection_previous,
+                ["<C-f>"] = actions.to_fuzzy_refine
             },
         },
     },
@@ -86,6 +87,7 @@ vim.keymap.set('n', '<leader>/', function()
     })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
+vim.keymap.set('n', '<leader>sa', builtin.resume, { desc = '[S]earch [A]gain' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
