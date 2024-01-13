fcb67abaed00a1114258f75bdc61b4fdbbb4637a
Olle <ollelundmark89@gmail.com>
2024-01-13 16:39:40 +0100

    Adding luasnip dependency to neogen

--- nvim/lua/feferoni/plugins/neogen.lua
+++ nvim/lua/feferoni/plugins/neogen.lua
@@ -1,19 +1,20 @@
 return {
     "danymat/neogen",
     dependencies = {
+        'L3MON4D3/LuaSnip',
         "nvim-treesitter/nvim-treesitter",
     },
     config = function()
         require('neogen').setup({ snippet_engine = "luasnip" })
 
-        vim.keymap.set('n', '<leader>cf', function ()
+        vim.keymap.set('n', '<leader>cf', function()
             require('neogen').generate({ type = 'func' })
         end, { desc = "[C]omment [F]ile", noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>cc', function ()
+        vim.keymap.set('n', '<leader>cc', function()
             require('neogen').generate({ type = 'class' })
         end, { desc = "[C]omment [C]lass", noremap = true, silent = true })
-        vim.keymap.set('n', '<leader>ct', function ()
+        vim.keymap.set('n', '<leader>ct', function()
             require('neogen').generate({ type = 'type' })
         end, { desc = "[C]omment [T]ype", noremap = true, silent = true })
-   end,
+    end,
 }
