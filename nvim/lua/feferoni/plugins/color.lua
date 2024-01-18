04cd263edee40152510a7b5828041a0794d90b51
Olle <ollelundmark89@gmail.com>
2024-01-18 10:51:31 +0100

    Changing how to set nvim theme

--- nvim/lua/feferoni/plugins/color.lua
+++ nvim/lua/feferoni/plugins/color.lua
@@ -4,14 +4,9 @@ local function SetColorScheme()
     vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#83a598" })
     vim.api.nvim_set_hl(0, "CopilotAnnotation", { fg = "#83a598" })
 
-    local fn = vim.fn
-    local cmd = vim.cmd
-    local set_theme_path = "$HOME/.config/tinted-theming/set_theme.lua"
-    local is_set_theme_file_readable = fn.filereadable(fn.expand(set_theme_path)) == 1 and true or false
-
-    if is_set_theme_file_readable then
-        cmd("let base16colorspace=256")
-        cmd("source " .. set_theme_path)
+    local theme = os.getenv("BASE16_THEME")
+    if theme then
+        vim.cmd('colorscheme base16-' .. theme)
     end
 end
 
