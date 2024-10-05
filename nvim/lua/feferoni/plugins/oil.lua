add4ac9ede6b5c84670fefa0274b879c81ab7118
Olle <ollelundmark89@gmail.com>
2024-10-06 00:30:12 +0200

    Adding oil to try it out

---
+++ nvim/lua/feferoni/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', },
+    config = function()
+        require('oil').setup({
+            default_file_explorer = true,
+            columns = { "icon" },
+            keymaps = {
+                ["<C-h>"] = false,
+                ["<C-l>"] = false,
+                ["<M-h>"] = "actions.select_split"
+            },
+            view_options = {
+                show_hidden = true,
+            },
+        })
+        vim.keymap.set("n", "<leader>_", function() vim.cmd("Oil") end, { desc = "Open parent directory" })
+        vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
+    end
+}
