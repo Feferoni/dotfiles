48f34b03da674bfc8b8cd78043754ad212f497cd
Olle <ollelundmark89@gmail.com>
2024-10-13 19:25:43 +0200

    Dap keybind

--- nvim/lua/feferoni/plugins/dap.lua
+++ nvim/lua/feferoni/plugins/dap.lua
@@ -209,6 +209,7 @@ return {
         vim.keymap.set("n", "<F1>", ":lua require'dap'.step_into()<CR>")
         vim.keymap.set("n", "<F2>", ":lua require'dap'.step_over()<CR>")
         vim.keymap.set("n", "<F3>", ":lua require'dap'.step_out()<CR>")
+        vim.keymap.set("n", "<F4>", ":lua require'dap'.continue()<CR>")
         vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
         vim.keymap.set("n", "<leader>td", ":lua require'dap-go'.debug_test()<CR>")
 
