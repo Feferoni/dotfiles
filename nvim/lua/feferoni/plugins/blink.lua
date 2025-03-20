8458f42ae2d187fae65bdfb2dd8931bb63ef9163
Olle <ollelundmark89@gmail.com>
2025-03-20 13:01:20 +0100

    Updating harpoon settings

--- nvim/lua/feferoni/plugins/blink.lua
+++ nvim/lua/feferoni/plugins/blink.lua
@@ -88,7 +88,7 @@ return {
             -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
             --
             -- See the fuzzy documentation for more information
-            fuzzy = { implementation = "prefer_rust_with_warning" },
+            fuzzy = { implementation = "prefer_rust" },
             snippets = {
                 preset = 'luasnip' -- 'default' 'luasnip' 'mini_snippets'
             },
