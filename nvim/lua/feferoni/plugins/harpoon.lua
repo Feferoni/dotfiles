ffced98f2bc076f439324b22f1b98e8e08c06679
elunoll <olle.lundmark@ericsson.com>
2025-03-21 16:31:34 +0100

    adding missing harpoon setup

--- nvim/lua/feferoni/plugins/harpoon.lua
+++ nvim/lua/feferoni/plugins/harpoon.lua
@@ -5,6 +5,9 @@ return {
     requires = { { "nvim-lua/plenary.nvim" } },
     config = function()
         local harpoon = require("harpoon")
+
+        harpoon:setup()
+
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
