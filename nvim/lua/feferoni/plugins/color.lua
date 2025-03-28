94db9909fb93394b3340261d8d17c8b0b4ce9aef
elunoll <olle.lundmark@ericsson.com>
2025-03-28 11:21:43 +0100

    Fixing color loading

--- nvim/lua/feferoni/plugins/color.lua
+++ nvim/lua/feferoni/plugins/color.lua
@@ -1,12 +1,15 @@
-local function SetColorScheme()
-    local config_path = vim.env.XDG_CONFIG_PATH or vim.fn.expand("~/.config/")
-    local set_theme_path = config_path .. "/tinted-theming/set_theme.lua"
+local function getThemePath()
+    local config_path = vim.fn.isdirectory("/repo/.config") == 1 and "/repo/.config" or vim.fn.expand("~/.config")
+    return config_path .. "/tinted-theming/set_theme.lua"
+end
 
-    local is_set_theme_file_readable = vim.fn.filereadable(vim.fn.expand(set_theme_path)) == 1 and true or false
+local function setColorScheme()
+    local theme_path = getThemePath()
 
+    local is_set_theme_file_readable = vim.fn.filereadable(vim.fn.expand(theme_path)) == 1 and true or false
     if is_set_theme_file_readable then
         vim.cmd("let base16colorspace=256")
-        vim.cmd("source " .. set_theme_path)
+        vim.cmd("source " .. theme_path)
     end
 
     vim.api.nvim_set_hl(0, "MatchParen", { fg = "#f0f0f0", bg = "#404040", bold = true })
@@ -19,6 +22,6 @@ return {
     'tinted-theming/tinted-vim',
     priority = 1000,
     config = function()
-        SetColorScheme()
+        setColorScheme()
     end
 }
