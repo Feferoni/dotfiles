021f71cb34b557d5a2421f384204073cd63fcb67
Olle <ollelundmark89@gmail.com>
2025-09-29 17:21:48 +0200

    Some more fixes to lsp

--- nvim/lua/feferoni/core/lsp.lua
+++ nvim/lua/feferoni/core/lsp.lua
@@ -15,14 +15,10 @@ vim.diagnostic.config({
     virtual_text = false,
 })
 
-vim.lsp.enable({
-    'bashls',
-    'clangd',
-    'cmake',
-    'gopls',
-    'jasonls',
-    'luals',
-    'marksman',
-    'pyright',
-    'zls',
-})
+local lsp_configs = {}
+for _, f in pairs(vim.api.nvim_get_runtime_file('lsp/*.lua', true)) do
+    local server_name = vim.fn.fnamemodify(f, ':t:r')
+    table.insert(lsp_configs, server_name)
+end
+
+vim.lsp.enable(lsp_configs)
