25b994b2b14e46f8699ed7c7ba50c70f30edc927
Olle <ollelundmark89@gmail.com>
2026-06-05 13:13:54 +0200

    Adding autocmd to solve copy buffer overwrite when snippet expansion happens

--- nvim/lua/feferoni/core/autocmds/autocmds.lua
+++ nvim/lua/feferoni/core/autocmds/autocmds.lua
@@ -7,6 +7,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
+-- Prevent snippet placeholder replacements from overwriting the unnamed register
+local select_reg_group = vim.api.nvim_create_augroup('preserve-reg-in-select', { clear = true })
+vim.api.nvim_create_autocmd("ModeChanged", {
+    group = select_reg_group,
+    pattern = "*:s",
+    callback = function()
+        vim.g._sel_reg = vim.fn.getreg('"')
+        vim.g._sel_reg_type = vim.fn.getregtype('"')
+    end,
+})
+vim.api.nvim_create_autocmd("ModeChanged", {
+    group = select_reg_group,
+    pattern = "s:*",
+    callback = function()
+        vim.schedule(function()
+            if vim.g._sel_reg then
+                vim.fn.setreg('"', vim.g._sel_reg, vim.g._sel_reg_type)
+            end
+        end)
+    end,
+})
+
 ---@diagnostic disable-next-line: missing-fields
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = { "*" },
